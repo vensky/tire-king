db8e2fb2ad33b288a53118a94ef00ee339163d40
vensky <ivan.fedotovskikh@gmail.com>
2021-06-15 14:29:47 +0500

    Добавляет раздел со слайдером

--- gulp/js.js
+++ gulp/js.js
@@ -1,6 +1,7 @@
 const { dest, src } = require('gulp');
 const multipipe = require('multipipe');
 const rename = require('gulp-rename');
+const include = require('gulp-file-include');
 
 const babel = require('gulp-babel');
 const eslint = require('gulp-eslint');
@@ -12,6 +13,7 @@ module.exports = function script(env='dev', version) {
         case 'dev':
             return multipipe(
                 src('src/js/app.js'),
+                include(),
                 sourcemaps.init(),
                 babel(),
                 sourcemaps.write(),
@@ -29,6 +31,7 @@ module.exports = function script(env='dev', version) {
         case 'build':
             return multipipe(
                 src('src/js/app.js'),
+                include(),
                 babel(),
                 terser(),
                 rename(`scripts.min-v${version}.js`),
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,15 @@
+'use strict';
 
+@@include('_lib/swiper-bundle.js')
+
+const swiper = new Swiper('.swiper-container', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
+});
